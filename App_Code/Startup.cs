0bcccd4705272c3accc66f7c596a41ff0aa50a58
mati87-87 <mati87-87@o2.pl>
2018-09-16 17:31:57 +0200

    a

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mateusz.Startup))]
+namespace mateusz
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
